a2f924a47358ddd369f0b514f08810f71ec21cf2
raghuatharva <raghuatharva95@gmail.com>
2025-04-07 11:29:51 +0530

    additional changes

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,16 @@
+def get getAccountID(string environment) {
+    switch (environment) {
+        case 'dev':
+            return '180294178330'
+        case 'qa':
+            return '180294178330'
+        case 'uat':
+            return '180294178330'
+        case 'pre-prod':
+            return '180294178330'
+        case 'prod':
+            return '180294178330'
+        default:
+            return 'nothing'
+    }
+}
